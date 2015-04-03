571908d78581b483a72e68d1a99a7e1ae1aedc64
Kamil Khamitov <berserq0123@gmail.com>
2015-04-03 17:52:46 +0300

    Refactor fileinfo class

--- Source/fileinfo.cpp
+++ Source/fileinfo.cpp
@@ -1,9 +1,12 @@
-#include "fileinfo.h"
 #include <QCryptographicHash>
+#include "fileinfo.h"
+#include "generalexception.h"
 QString FileInfo::getMD5() {
     if (m_md5.isEmpty()) {
         QFile in(m_fileinfo.absoluteFilePath());
-        in.open(QIODevice::ReadOnly);
+        if (!in.open(QIODevice::ReadOnly)) {
+            throw GeneralException(tr("Cant open file %1").arg(m_fileinfo.absoluteFilePath()));
+        }
         QByteArray buff = in->readAll();
         in.close();
         m_md5 = QString(QCryptographicHash::hash(buff,QCryptographicHash::Md5));
